dee88ec1583695a383f2cc86a35c320075652678
vilvaathibanpb <vilvaathiban@gmail.com>
2023-07-07 02:41:41 +0200

    fix shasum

--- Formula/clean-local.rb
+++ Formula/clean-local.rb
@@ -2,7 +2,7 @@ class CleanLocal < Formula
   desc "CLI for deleting all the files in .gitignore (including globs)"
   homepage "https://github.com/vilvaathibanpb/clean-local"
   url "https://github.com/vilvaathibanpb/clean-local/releases/download/v1.0.0/clean-local-1.0.0-x86_64-apple-darwin.tar.gz"
-  sha256 "82411dada0eb2934584ea780d117c19a387f6b01"
+  sha256 "fc84e81fa80a5da5f213dcea6c4b340a81e216e57c9ca26a3623feff273c525e"
   version "1.0.0"
 
   def install
